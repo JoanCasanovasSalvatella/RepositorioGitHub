a48475b508066a894014a134250d3cfdf48a3e2c
JoanDev <joancasanovassalvatella@gmail.com>
2024-09-20 17:49:11 +0200

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Este es el primer documento");
+	}
+
+}
