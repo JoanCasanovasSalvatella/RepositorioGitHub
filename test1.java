da4715ad2882debe75409c9af45d9f52d656de57
aroqiler <aroq20020405@alumnes.ilerna.com>
2024-09-23 16:42:40 +0200

    Update test1.java

--- test1.java
+++ test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Este es el primer documento");
+		
+		System.out.print("Esta es la segunda linea del primer documento.");
 	}
-
 }
